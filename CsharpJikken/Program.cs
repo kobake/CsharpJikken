96d47ed1bcf90f2bb53c6e7b33084066049aa74e
kobake <kobake@users.sourceforge.net>
2017-01-16 00:28:29 +0900

    Add project files.

---
+++ CsharpJikken/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpJikken
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
