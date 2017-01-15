e05977bb469b5aee358eef5b12eea38323045c4b
kobake <kobake@users.sourceforge.net>
2017-01-16 00:29:55 +0900

    hello datetime

--- CsharpJikken/Program.cs
+++ CsharpJikken/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CsharpJikken
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello " + DateTime.Now);
+            File.WriteAllText("hoge.txt", "world + " + DateTime.Now + "\n");
+        }
+    }
 }
